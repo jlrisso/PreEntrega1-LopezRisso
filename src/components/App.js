0fd801bd959dd7eceba71eb9745c1bfe4d14644f
jlrissom <jal.risso@gmail.com>
2022-11-05 19:29:10 -0300

    context Out! II

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,14 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import {BrowserRouter} from 'react-router-dom';
-import CartProvider from '../context/CartContext';
+
 
 function App() {
   return (
     <BrowserRouter>
-      <CartProvider>
           <Header />
           <Main />
           <Footer />
-      </CartProvider>
     </BrowserRouter>
   );
 }
